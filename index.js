2248962dcfb009a029a7548d7ccd3d4f0b450359
delvedor <tommydelved@gmail.com>
2017-10-11 17:33:17 +0200

    Added pg.query utility

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const fp = require('fastify-plugin')
+const promisify = require('util.promisify')
 const pg = require('pg')
 
 function fastifyPostgres (fastify, options, next) {
@@ -9,8 +10,33 @@ function fastifyPostgres (fastify, options, next) {
   fastify.decorate('pg', {
     connect: pool.connect.bind(pool),
     pool: pool,
-    Client: pg.Client
+    Client: pg.Client,
+    query: promisify(query)
   })
+
+  function query (text, value, callback) {
+    if (typeof value === 'function') {
+      callback = value
+      value = null
+    }
+
+    pool.connect(onConnect)
+
+    function onConnect (err, client, release) {
+      if (err) return callback(err)
+
+      if (value) {
+        client.query(text, value, onResult)
+      } else {
+        client.query(text, onResult)
+      }
+
+      function onResult (err, result) {
+        release()
+        callback(err, result)
+      }
+    }
+  }
 
   fastify.addHook('onClose', onClose)
 
