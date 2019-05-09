58516ead1cad7f0dfb78480c8f06ceba2c1d572c
darkgl0w <31093081+darkgl0w@users.noreply.github.com>
2019-05-09 19:43:23 +0200

    tests rework and code clean up
    - Code coverage raised to 94.83%

---
+++ test/query.test.js
@@ -0,0 +1,263 @@
+'use strict'
+
+const t = require('tap')
+const test = t.test
+const Fastify = require('fastify')
+const fastifyPostgres = require('../index')
+
+test('When fastify.pg root namespace is used:', (childTest) => {
+  childTest.test('Should be able to connect and perform a query with a callback', (t) => {
+    t.plan(4)
+
+    const fastify = Fastify()
+    t.teardown(() => fastify.close())
+
+    fastify.register(fastifyPostgres, {
+      connectionString: 'postgres://postgres@localhost/postgres'
+    })
+
+    fastify.ready((err) => {
+      t.error(err)
+
+      fastify.pg.connect(onConnect)
+    })
+
+    function onConnect (err, client, done) {
+      t.error(err)
+
+      client.query('SELECT NOW()', (err, result) => {
+        done()
+        t.error(err)
+        t.ok(result.rows)
+      })
+    }
+  })
+
+  childTest.test('Should be able to use the query util with a callback', (t) => {
+    t.plan(3)
+
+    const fastify = Fastify()
+    t.teardown(() => fastify.close())
+
+    fastify.register(fastifyPostgres, {
+      connectionString: 'postgres://postgres@localhost/postgres'
+    })
+
+    fastify.ready((err) => {
+      t.error(err)
+
+      fastify.pg.query('SELECT NOW()', (err, result) => {
+        t.error(err)
+        t.ok(result.rows)
+      })
+    })
+  })
+
+  childTest.test('Should be able to use the query util with promises', (t) => {
+    t.plan(2)
+
+    const fastify = Fastify()
+    t.teardown(() => fastify.close())
+
+    fastify.register(fastifyPostgres, {
+      connectionString: 'postgres://postgres@localhost/postgres'
+    })
+
+    fastify.ready((err) => {
+      t.error(err)
+
+      fastify.pg
+        .query('SELECT NOW()')
+        .then((result) => {
+          t.ok(result.rows)
+        })
+        .catch((err) => {
+          t.fail(err)
+        })
+    })
+  })
+
+  childTest.test(
+    'query util should return an error when pg fails to perform an operation using a callback',
+    (t) => {
+      t.plan(4)
+
+      const fastify = Fastify()
+      t.teardown(() => fastify.close())
+
+      const DB_NAME = 'database_that_do_not_exist'
+
+      fastify.register(fastifyPostgres, {
+        connectionString: `postgres://postgres@localhost/${DB_NAME}`
+      })
+
+      fastify.ready((err) => {
+        t.error(err)
+
+        fastify.pg.query('SELECT NOW()', (err, result) => {
+          t.is(result, undefined)
+          t.ok(err)
+          t.is(err.message, `database "${DB_NAME}" does not exist`)
+        })
+      })
+    }
+  )
+
+  childTest.test('Should throw when pg fails to perform operation with promises', (t) => {
+    t.plan(3)
+
+    const fastify = Fastify()
+    t.teardown(() => fastify.close())
+
+    const DB_NAME = 'database_that_do_not_exist'
+
+    fastify.register(fastifyPostgres, {
+      connectionString: `postgres://postgres@localhost/${DB_NAME}`
+    })
+
+    fastify.ready((err) => {
+      t.error(err)
+
+      fastify.pg
+        .query('SELECT NOW()')
+        .then((result) => {
+          t.fail(result)
+        })
+        .catch((err) => {
+          t.ok(err)
+          t.is(err.message, `database "${DB_NAME}" does not exist`)
+        })
+    })
+  })
+
+  childTest.end()
+})
+
+test('When a fastify.pg namespace is used:', (childTest) => {
+  childTest.test('Should be able to connect and perform a query', (t) => {
+    t.plan(4)
+
+    const fastify = Fastify()
+    t.teardown(() => fastify.close())
+
+    fastify.register(fastifyPostgres, {
+      connectionString: 'postgres://postgres@localhost/postgres',
+      name: 'test'
+    })
+
+    fastify.ready((err) => {
+      t.error(err)
+      fastify.pg.test.connect(onConnect)
+    })
+
+    function onConnect (err, client, done) {
+      t.error(err)
+      client.query('SELECT NOW()', (err, result) => {
+        done()
+        t.error(err)
+        t.ok(result.rows)
+      })
+    }
+  })
+
+  childTest.test('Should be able to use query util with a callback', (t) => {
+    t.plan(3)
+
+    const fastify = Fastify()
+    t.teardown(() => fastify.close())
+
+    fastify.register(fastifyPostgres, {
+      connectionString: 'postgres://postgres@localhost/postgres',
+      name: 'test'
+    })
+
+    fastify.ready((err) => {
+      t.error(err)
+      fastify.pg.test.query('SELECT NOW()', (err, result) => {
+        t.error(err)
+        t.ok(result.rows)
+      })
+    })
+  })
+
+  childTest.test('Should be able to use query util with promises', (t) => {
+    t.plan(2)
+
+    const fastify = Fastify()
+    t.teardown(() => fastify.close())
+
+    fastify.register(fastifyPostgres, {
+      connectionString: 'postgres://postgres@localhost/postgres',
+      name: 'test'
+    })
+
+    fastify.ready((err) => {
+      t.error(err)
+
+      fastify.pg.test
+        .query('SELECT NOW()')
+        .then((result) => {
+          t.ok(result.rows)
+        })
+        .catch((err) => {
+          t.fail(err)
+        })
+    })
+  })
+
+  childTest.test('Should be able to use native module', (t) => {
+    t.plan(2)
+
+    const fastify = Fastify()
+    t.teardown(() => fastify.close())
+
+    fastify.register(fastifyPostgres, {
+      connectionString: 'postgres://postgres@localhost/postgres',
+      name: 'test',
+      native: true
+    })
+
+    fastify.ready((err) => {
+      t.error(err)
+
+      fastify.pg.test
+        .query('SELECT 1 AS one')
+        .then((result) => {
+          t.is(result.rows[0].one, 1)
+        })
+        .catch((err) => {
+          t.fail(err)
+        })
+    })
+  })
+
+  childTest.test('Should throw when pg fails to perform an operation with promises', (t) => {
+    t.plan(3)
+
+    const fastify = Fastify()
+    t.teardown(() => fastify.close())
+
+    const DB_NAME = 'database_that_do_not_exist'
+
+    fastify.register(fastifyPostgres, {
+      connectionString: `postgres://postgres@localhost/${DB_NAME}`,
+      name: 'test'
+    })
+
+    fastify.ready((err) => {
+      t.error(err)
+
+      fastify.pg.test
+        .query('SELECT NOW()')
+        .then((result) => {
+          t.fail(result)
+        })
+        .catch((err) => {
+          t.ok(err)
+          t.is(err.message, `database "${DB_NAME}" does not exist`)
+        })
+    })
+  })
+
+  childTest.end()
+})
---
+++ test/transaction.test.js
@@ -0,0 +1,507 @@
+'use strict'
+
+const t = require('tap')
+const test = t.test
+const Fastify = require('fastify')
+const fastifyPostgres = require('../index')
+
+test('When fastify.pg root namespace is used:', (childTest) => {
+  childTest.test('Should be able to use transact util with a callback', (t) => {
+    t.plan(3)
+
+    const fastify = Fastify()
+    t.teardown(() => fastify.close())
+
+    fastify.register(fastifyPostgres, {
+      connectionString: 'postgres://postgres@localhost/postgres'
+    })
+
+    fastify.ready((err) => {
+      t.error(err)
+
+      fastify.pg.transact(
+        (client) =>
+          client.query('INSERT INTO users(username) VALUES($1) RETURNING id', [
+            'root-with-callback'
+          ]),
+        function (err, result) {
+          t.error(err)
+          t.is(result.rows.length, 1)
+
+          const userId = result.rows[0].id
+
+          fastify.pg
+            .query('SELECT * FROM users WHERE id = $1', [userId])
+            .then((result) => {
+              t.is(result.rows[0].username, 'root-with-callback')
+            })
+            .catch((err) => {
+              t.fail(err)
+            })
+        }
+      )
+    })
+  })
+
+  childTest.test('Should be able to use transact util with promises', (t) => {
+    t.plan(3)
+
+    const fastify = Fastify()
+    t.teardown(() => fastify.close())
+
+    fastify.register(fastifyPostgres, {
+      connectionString: 'postgres://postgres@localhost/postgres'
+    })
+
+    fastify.ready((err) => {
+      t.error(err)
+
+      fastify.pg
+        .transact((client) =>
+          client.query('INSERT INTO users(username) VALUES($1) RETURNING id', ['root-with-promise'])
+        )
+        .then((result) => {
+          t.is(result.rows.length, 1)
+
+          const userId = result.rows[0].id
+
+          fastify.pg
+            .query('SELECT * FROM users WHERE id = $1', [userId])
+            .then((result) => {
+              t.is(result.rows[0].username, 'root-with-promise')
+            })
+            .catch((err) => {
+              t.fail(err)
+            })
+        })
+        .catch((err) => {
+          t.fail(err)
+        })
+    })
+  })
+
+  childTest.test('Should be able to use transact util with a commit callback', (t) => {
+    t.plan(4)
+
+    const fastify = Fastify()
+    t.teardown(() => fastify.close())
+
+    fastify.register(fastifyPostgres, {
+      connectionString: 'postgres://postgres@localhost/postgres'
+    })
+
+    fastify.ready((err) => {
+      t.error(err)
+
+      fastify.pg.transact(
+        (client, commit) =>
+          client.query(
+            'INSERT INTO users(username) VALUES($1) RETURNING id',
+            ['root-commit-callback'],
+            (err, id) => {
+              commit(err, id)
+            }
+          ),
+        function (err, result) {
+          t.error(err)
+          t.is(result.rows.length, 1)
+
+          const userId = result.rows[0].id
+
+          fastify.pg
+            .query('SELECT * FROM users WHERE id = $1', [userId])
+            .then((result) => {
+              t.is(result.rows[0].username, 'root-commit-callback')
+            })
+            .catch((err) => {
+              t.fail(err)
+            })
+        }
+      )
+    })
+  })
+
+  childTest.test('Should trigger a rollback when something goes wrong (with callback)', (t) => {
+    t.plan(9)
+
+    const fastify = Fastify()
+    t.teardown(() => fastify.close())
+
+    fastify.register(fastifyPostgres, {
+      connectionString: 'postgres://postgres@localhost/postgres'
+    })
+
+    fastify.ready((err) => {
+      t.error(err)
+
+      fastify.pg.transact(
+        (client) => {
+          return client
+            .query('INSERT INTO users(username) VALUES($1) RETURNING id', [
+              'root-rollback-user-callback'
+            ])
+            .then((result) => {
+              t.ok(result)
+
+              const userId = result.rows[0].id
+
+              return client
+                .query('SELECT * FROM users WHERE id = $1', [userId])
+                .then((result) => {
+                  t.ok(result)
+                  t.is(result.rows[0].username, 'root-rollback-user-callback')
+                })
+                .then(() => {
+                  throw new Error('We make it throw on purpose to trigger a rollback')
+                })
+            })
+        },
+        function (err, result) {
+          t.ok(err)
+          t.is(err.message, 'We make it throw on purpose to trigger a rollback')
+          t.is(result, undefined)
+
+          fastify.pg
+            .query(`SELECT * FROM users WHERE username = 'root-rollback-user-callback'`)
+            .then((result) => {
+              t.ok(result)
+              t.is(result.rows.length, 0)
+            })
+            .catch((err) => {
+              t.fail(err)
+            })
+        }
+      )
+    })
+  })
+
+  childTest.test('Should trigger a rollback when something goes wrong (with promises)', (t) => {
+    t.plan(8)
+
+    const fastify = Fastify()
+    t.teardown(() => fastify.close())
+
+    fastify.register(fastifyPostgres, {
+      connectionString: 'postgres://postgres@localhost/postgres'
+    })
+
+    fastify.ready((err) => {
+      t.error(err)
+
+      fastify.pg
+        .transact((client) =>
+          client
+            .query('INSERT INTO users(username) VALUES($1) RETURNING id', [
+              'root-rollback-user-promise'
+            ])
+            .then((result) => {
+              t.ok(result)
+
+              const userId = result.rows[0].id
+
+              return client
+                .query('SELECT * FROM users WHERE id = $1', [userId])
+                .then((result) => {
+                  t.ok(result)
+                  t.is(result.rows[0].username, 'root-rollback-user-promise')
+                })
+                .then(() => {
+                  throw new Error('We make it throw on purpose to trigger a rollback')
+                })
+            })
+        )
+        .catch((err) => {
+          t.ok(err)
+          t.is(err.message, 'We make it throw on purpose to trigger a rollback')
+
+          fastify.pg
+            .query(`SELECT * FROM users WHERE username = 'root-rollback-user-promise'`)
+            .then((result) => {
+              t.ok(result)
+              t.is(result.rows.length, 0)
+            })
+            .catch((err) => {
+              t.fail(err)
+            })
+        })
+    })
+  })
+
+  childTest.test('Should throw if the pool connection throws an error', (t) => {
+    t.plan(3)
+
+    const fastify = Fastify()
+    const BAD_DB_NAME = 'db_that_does_not_exist'
+
+    fastify.register(fastifyPostgres, {
+      connectionString: `postgres://postgres@localhost/${BAD_DB_NAME}`
+    })
+
+    fastify.ready((err) => {
+      t.error(err)
+
+      fastify.pg.transact((client) => client.query('SELECT NOW()'))
+        .catch((err) => {
+          t.ok(err)
+          t.is(err.message, `database "${BAD_DB_NAME}" does not exist`)
+        })
+    })
+  })
+
+  childTest.end()
+})
+
+test('When fastify.pg.test namespace is used:', (childTest) => {
+  childTest.test('Should be able to use transact util with a callback', (t) => {
+    t.plan(4)
+
+    const fastify = Fastify()
+    t.teardown(() => fastify.close())
+
+    fastify.register(fastifyPostgres, {
+      connectionString: 'postgres://postgres@localhost/postgres',
+      name: 'test'
+    })
+
+    fastify.ready((err) => {
+      t.error(err)
+
+      fastify.pg.test.transact(
+        (client) =>
+          client.query('INSERT INTO users(username) VALUES($1) RETURNING id', [
+            'namespace-with-callback'
+          ]),
+        function (err, result) {
+          t.error(err)
+          t.is(result.rows.length, 1)
+
+          const userId = result.rows[0].id
+
+          fastify.pg.test
+            .query('SELECT * FROM users WHERE id = $1', [userId])
+            .then((result) => {
+              t.is(result.rows[0].username, 'namespace-with-callback')
+            })
+            .catch((err) => {
+              t.fail(err)
+            })
+        }
+      )
+    })
+  })
+
+  childTest.test('Should be able to use transact util with promises', (t) => {
+    t.plan(3)
+
+    const fastify = Fastify()
+    t.teardown(() => fastify.close())
+
+    fastify.register(fastifyPostgres, {
+      connectionString: 'postgres://postgres@localhost/postgres',
+      name: 'test'
+    })
+
+    fastify.ready((err) => {
+      t.error(err)
+
+      fastify.pg.test
+        .transact((client) =>
+          client.query('INSERT INTO users(username) VALUES($1) RETURNING id', [
+            'namespace-with-promise'
+          ])
+        )
+        .then((result) => {
+          t.is(result.rows.length, 1)
+
+          const userId = result.rows[0].id
+
+          fastify.pg.test
+            .query('SELECT * FROM users WHERE id = $1', [userId])
+            .then((result) => {
+              t.is(result.rows[0].username, 'namespace-with-promise')
+            })
+            .catch((err) => {
+              t.fail(err)
+            })
+        })
+        .catch((err) => {
+          t.fail(err)
+        })
+    })
+  })
+
+  childTest.test('Should be able to use transact util with a commit callback', (t) => {
+    t.plan(4)
+
+    const fastify = Fastify()
+    t.teardown(() => fastify.close())
+
+    fastify.register(fastifyPostgres, {
+      connectionString: 'postgres://postgres@localhost/postgres',
+      name: 'test'
+    })
+
+    fastify.ready((err) => {
+      t.error(err)
+
+      fastify.pg.test.transact(
+        (client, commit) => {
+          client.query(
+            'INSERT INTO users(username) VALUES($1) RETURNING id',
+            ['namespace-commit-callback'],
+            (err, id) => {
+              commit(err, id)
+            }
+          )
+        },
+        function (err, result) {
+          t.error(err)
+          t.is(result.rows.length, 1)
+
+          const userId = result.rows[0].id
+
+          fastify.pg.test
+            .query('SELECT * FROM users WHERE id = $1', [userId])
+            .then((result) => {
+              t.is(result.rows[0].username, 'namespace-commit-callback')
+            })
+            .catch((err) => {
+              t.fail(err)
+            })
+        }
+      )
+    })
+  })
+
+  childTest.test('Should trigger a rollback when something goes wrong (with callback)', (t) => {
+    t.plan(9)
+
+    const fastify = Fastify()
+    t.teardown(() => fastify.close())
+
+    fastify.register(fastifyPostgres, {
+      connectionString: 'postgres://postgres@localhost/postgres',
+      name: 'test'
+    })
+
+    fastify.ready((err) => {
+      t.error(err)
+
+      fastify.pg.test.transact(
+        (client) => {
+          return client
+            .query('INSERT INTO users(username) VALUES($1) RETURNING id', [
+              'namespace-rollback-user-callback'
+            ])
+            .then((result) => {
+              t.ok(result)
+
+              const userId = result.rows[0].id
+
+              return client
+                .query('SELECT * FROM users WHERE id = $1', [userId])
+                .then((result) => {
+                  t.ok(result)
+                  t.is(result.rows[0].username, 'namespace-rollback-user-callback')
+                })
+                .then(() => {
+                  throw new Error('We make it throw on purpose to trigger a rollback')
+                })
+            })
+        },
+        function (err, result) {
+          t.ok(err)
+          t.is(err.message, 'We make it throw on purpose to trigger a rollback')
+          t.is(result, undefined)
+
+          fastify.pg.test
+            .query(`SELECT * FROM users WHERE username = 'namespace-rollback-user-callback'`)
+            .then((result) => {
+              t.ok(result)
+              t.is(result.rows.length, 0)
+            })
+            .catch((err) => {
+              t.fail(err)
+            })
+        }
+      )
+    })
+  })
+
+  childTest.test('Should trigger a rollback when something goes wrong (with promises)', (t) => {
+    t.plan(8)
+
+    const fastify = Fastify()
+    t.teardown(() => fastify.close())
+
+    fastify.register(fastifyPostgres, {
+      connectionString: 'postgres://postgres@localhost/postgres',
+      name: 'test'
+    })
+
+    fastify.ready((err) => {
+      t.error(err)
+
+      fastify.pg.test
+        .transact((client) =>
+          client
+            .query('INSERT INTO users(username) VALUES($1) RETURNING id', [
+              'namespace-rollback-user-promise'
+            ])
+            .then((result) => {
+              t.ok(result)
+
+              const userId = result.rows[0].id
+
+              return client
+                .query('SELECT * FROM users WHERE id = $1', [userId])
+                .then((result) => {
+                  t.ok(result)
+                  t.is(result.rows[0].username, 'namespace-rollback-user-promise')
+                })
+                .then(() => {
+                  throw new Error('We make it throw on purpose to trigger a rollback')
+                })
+            })
+        )
+        .catch((err) => {
+          t.ok(err)
+          t.is(err.message, 'We make it throw on purpose to trigger a rollback')
+
+          fastify.pg.test
+            .query(`SELECT * FROM users WHERE username = 'namespace-rollback-user-promise'`)
+            .then((result) => {
+              t.ok(result)
+              t.is(result.rows.length, 0)
+            })
+            .catch((err) => {
+              t.fail(err)
+            })
+        })
+    })
+  })
+
+  childTest.test('Should throw if the pool connection throws an error', (t) => {
+    t.plan(3)
+
+    const fastify = Fastify()
+    const BAD_DB_NAME = 'db_that_does_not_exist'
+
+    fastify.register(fastifyPostgres, {
+      connectionString: `postgres://postgres@localhost/${BAD_DB_NAME}`,
+      name: 'test'
+    })
+
+    fastify.ready((err) => {
+      t.error(err)
+
+      fastify.pg.test.transact((client) => client.query('SELECT NOW()'))
+        .catch((err) => {
+          t.ok(err)
+          t.is(err.message, `database "${BAD_DB_NAME}" does not exist`)
+        })
+    })
+  })
+
+  childTest.end()
+})
