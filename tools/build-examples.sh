9e1ca07599e05763c01533d39fd90c985135643e
Evgeny Markov <evgenymarkov@yandex.ru>
2020-08-23 01:07:36 +0300

    fix: make type checking faster and more portable

--- tools/build-examples.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-set -ex
-
-npx tsc --project ./examples/typescript/single-db/tsconfig.json
-npx tsc --project ./examples/typescript/multiple-db/tsconfig.json
-npx tsc --project ./examples/typescript/transactions/tsconfig.json
